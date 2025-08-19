c98c1b351a55cb26b1c42aec2640efed746475a7
joaaoow <joaopsilvapereira1701@gmail.com>
2025-08-19 13:24:11 -0300

    alterando os arquivos

--- exercicio_02.js
+++ exercicio_02.js
@@ -1,27 +1,39 @@
+       
+        function jogarJokenPo(){
+            //Decladrando as variáveis
+        var maoUsuario;
+        var maoMaquina = Math.floor(Math.random() * 3) + 1; // 1: Pedra, 2: Papel, 3: Tesoura
+            //Prompt do usuário
+        maoUsuario = parseInt(window.prompt("Escolha: 1- Pedra\n 2- Papel\n 3- Tesoura"));    
+            //Verificando se a escolha é válida
+       if(isNaN(maoUsuario) || maoUsuario < 1 || maoUsuario > 3){
+        alert("Escolha inválida! Por favor, escolha 1, 2 ou 3.");
+        return; // Encerra a função se a escolha for inválida
+       }
+        //Mapeia os números para as mãos correspondentes
+        var jogadas = {
+            1: "Pedra",
+            2: "Papel",
+            3: "Tesoura"
+        };
+
+        console.log(`Você escolheu: ${jogadas[maoUsuario]}` );
+        console.log(`A máquina escolheu: ${jogadas[maoMaquina]}`);
     
-    var mao;
-    var maoMaquina = Math.floor(Math.random() * 4) + 1; // 1: Pedra, 2: Papel, 3: Tesoura
-    function verificarMao(){
-        
-        while(mao != maoMaquina){
-        mao = parseInt(window.prompt("Escolha Pedra, Papel ou Tesoura: "));    
-        if(mao === maoMaquina){
-            console.log("Empate");
-        } else if(mao === 1 && maoMaquina === 2 ||
-            mao === 2 && maoMaquina === 3 ||
-            mao === 1 && maoMaquina === 3
-        ){
-            console.log("Você perdeu");
-        } else if(mao === 2 && maoMaquina === 1 ||
-            mao === 3 && maoMaquina === 2 ||
-            mao === 3 && maoMaquina === 1){
-                console.log("Você ganhou");
-            }
-        console.log("Você escolheu: " + mao);
+            //Escolha do usuário
+        console.log("Você escolheu: " + maoUsuario);
+            //Escolha da máquina
         console.log("A máquina escolheu: " + maoMaquina);
         
+        if(maoUsuario === maoMaquina){
+            console.log("Empate!");
+        } else if(maoUsuario ===1 && maoMaquina === 3 ||
+                maoUsuario === 2 && maoMaquina === 1 ||
+                maoUsuario === 3 && maoMaquina === 2
+        ){
+            console.log("Você Ganhou!");
+        } else{
+            console.log("Você Perdeu!");
         }
-
-        verificarMao();
-        
     }
+     jogarJokenPo();
